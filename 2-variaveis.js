c9173fdfa50de1d2f1db5e636c5eb9f3d2eb415c
Yasmin Ferreira <yasmincferreira8@gmail.com>
2022-06-01 16:15:06 -0400

    Create 2-variaveis.js

---
+++ 2-variaveis.js
@@ -0,0 +1,11 @@
+console.log("Trabalhando com Variaveis");
+
+const idade = 18;
+const nome = "Yasmin";
+let ano = 2003;
+
+console.log(idade);
+console.log(nome);
+console.log(ano);
+
+console.log(idade, nome, ano);
