04d16bcc8e1d54bf2d03bafc1bc29f5ff04bbf17
Yasmin Ferreira <yasmincferreira8@gmail.com>
2022-06-08 10:22:08 -0400

    08-06-2022

--- 4-conversaoDeTipos.js
+++ 4-conversaoDeTipos.js
@@ -2,4 +2,10 @@ console.log("Conversão de Tipos");
 
 console.log("ano " + 2022);
 console.log("2" +"2");
-console.log(parseInt("2") + parseInt("2"));
+console.log(parseInt("2") + parseInt("2")); 
+
+console.log("10" / "2");
+console.log("7" / "2");// trabalha com numeros inteiros e decimais- ponto flutuante
+
+
+console.log("Yasmin" / "2"); //retorna NaN- Not a Number
