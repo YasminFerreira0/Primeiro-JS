8110a41f9777449dcb71b7f48df20934e14d9116
Yasmin Ferreira <yasmincferreira8@gmail.com>
2022-06-09 10:13:39 -0400

    Update 6-listas.js

--- 6-listas.js
+++ 6-listas.js
@@ -7,5 +7,12 @@ const listaDeDestinos = new Array(
 );
 
 listaDeDestinos.push(`Curitiba`);//.push: "empurra" adiciona mais um item a Array
+
 console.log("Destinos Possiveis");
 console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1, 1);//.splice: deleta um item da Array, sendo necessario indicar em qual item vai comçar a deletar "start"(a contagens de itens sempre começa do 0), e a quantidae de itens
+
+console.log(listaDeDestinos);
+
+console.log(listaDeDestinos[1]); //o couchetes "[]": especifica um item em uma posição especifica, imprimindo assim somente aquele item
